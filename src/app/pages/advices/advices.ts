7b7007809a1c23b37e4cbde66dae1f74991cf9e5
ODDT <vratislavino@gmail.com>
2019-09-27 21:46:16 +0200

    Advices ready for styling

--- src/app/pages/advices/advices.ts
+++ src/app/pages/advices/advices.ts
@@ -1,7 +1,8 @@
 import { PopoverController } from '@ionic/angular';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountProvider } from '../../providers/account/account';
 import { Router } from '@angular/router';
+import { QuoteProvider } from 'src/app/providers/quote/quote';
 
 /**
  * Generated class for the AdvicesPage page.
@@ -16,13 +17,40 @@ import { Router } from '@angular/router';
   templateUrl: 'advices.html',
   providers: [AccountProvider]
 })
-export class AdvicesPage {
+export class AdvicesPage implements OnInit {
 
-  constructor(public ac:AccountProvider, private router:Router) {
+  currentAdvice: string = "";
+  currentAdvisor: string = "";
+
+  constructor(public ac:AccountProvider, private router:Router, private qp: QuoteProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AdvicesPage');
+  ngOnInit(): void {
+    this.qp.getAdvice().subscribe((data) => {
+      if(data == "") {
+        this.currentAdvice = "";
+        this.currentAdvisor = "";
+      } else {
+        this.currentAdvice = data["text"];
+        this.currentAdvisor = data["advisor"];
+      }
+    });
+  }
+  
+  getNewAdvice(advisor) {
+    this.qp.getNewAdvice(advisor).subscribe((data) => {
+      if(data === true) {
+        this.ngOnInit();
+      }
+    });
+  }
+
+  getAdvisor() {
+    return this.currentAdvisor == "1" ? "Mamka" : "Přítel"; 
+  }
+
+  getAdvisorImage() {
+    return this.currentAdvisor == "1" ? "./assets/imgs/avatar2.png" : "./assets/imgs/avatar3.png"; 
   }
 
   adviceMother() {
