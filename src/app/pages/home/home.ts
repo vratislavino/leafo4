47977d7f42fc6ad75ea600cae1dfbec1c557203c
ODDT <vratislavino@gmail.com>
2019-10-26 20:13:08 +0200

    transfer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { ModalController, Platform } from '@ionic/angular';
 import { AccountProvider } from '../../providers/account/account';
 import { User } from '../../model/UserModel';
@@ -7,6 +7,8 @@ import { UserProvider } from 'src/app/providers/user/user';
 import { QuoteProvider } from 'src/app/providers/quote/quote';
 import { RatingProvider } from 'src/app/providers/rating/rating';
 import { NotificationProvider } from 'src/app/providers/notification/notification';
+import { LeafoInfoProvider } from 'src/app/providers/leafo-info/leafo-info';
+import { LeafoInfoType } from 'src/app/components/info-leafo/info-leafo';
 
 /**
  * Generated class for the HomePage page.
@@ -31,14 +33,15 @@ export class HomePage implements OnInit {
   private quoteObtained = false;
   private horoscopeObtained = false;
 
-
   constructor(private platform: Platform, 
     private router: Router, 
     public ac: AccountProvider, 
     public userService: UserProvider, 
     private qp: QuoteProvider, 
     private rp:RatingProvider,
-    private np:NotificationProvider) {
+    private np:NotificationProvider,
+    private lip: LeafoInfoProvider,
+    private vc: ViewContainerRef) {
   }
 
   ngOnInit(): void {
@@ -56,12 +59,68 @@ export class HomePage implements OnInit {
   }
 
   initData() {
+    this.qp.getHistoryQuotes(1).subscribe(data => console.log(data));
+
+
+    return;
+    var strs = [];
+    strs.push(this.initQuote());
+    strs.push(this.initRating());
+    strs.push(this.initMonday());
+    console.log(strs);
+    var str = ""
+    for(var i = 0; i < strs.length; i++) {
+      if(strs[i] != undefined) {
+        	str += strs[i] + "<br>";
+      }
+    }
+    /* 
+    "SELECT q.id_q AS id_q, q.quote AS quote, q.author AS author, q.date AS qdate, uq.faved AS faved FROM users_quotes AS uq JOIN users AS u ON uq.id_u=u.id_u JOIN quotes AS q ON uq.id_q=q.id_q WHERE uq.id_u=1 AND q.date  > 1569181760 ORDER BY date DESC LIMIT 1; Time: 1571773760"
+    */
+    this.lip.createAndShowLeafoBubble(this.vc, str,"Novinky!", LeafoInfoType.Happy);
+
     /*this.setNotifsForRating();
     this.setNotifsForQuote();
     this.*/
   }
 
-  
+  initQuote() {
+    this.getLastQuote().then(quote => {
+      console.log(quote);
+      if(quote["isNew"] == 1) { 
+        return "Na stránce citátů máš nový citát!";
+      } else {
+        return "";
+      }
+    }).catch(err => {
+      console.error(err);
+    });
+    
+  }
+
+  getLastQuote() {
+    return new Promise((resolve, reject)=> {
+      this.qp.getHistoryQuotes(1).subscribe(res => {
+        console.log(res);
+          if(res["length"] > 0)
+            resolve(res[0]);
+          else 
+            reject("Nemá ještě žádný citát!");
+      }, err => {
+        reject(err);
+      });
+    });
+  }  
+
+  initRating() {
+    this.rp.getDayData(new Date(),true).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  initMonday() {
+
+  }
 
   setNotifsForRating() {
     if(this.todayRated) {
